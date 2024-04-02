2dc7375193df9028c34150101d71119a198fa004
solo2112 <dawitabrham0021@gmail.com>
2024-04-03 01:02:57 +0300

    id field removed

--- core/serializers.py
+++ core/serializers.py
@@ -3,4 +3,4 @@ from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ['id', 'username', 'password']
+        fields = [ 'username', 'password']
