7434b73a2987db9aac2da970f1e179396a5aae79
solo2112 <dawitabrham0021@gmail.com>
2024-04-02 23:10:25 +0300

    core user primary key

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     middle_name = models.CharField(max_length=50, blank=True, null=True)
-    username = models.EmailField(unique=True)
+    username = models.EmailField(unique=True, primary_key=True)
     verfication_pin = models.IntegerField(null=True, blank=True)
     password_reset_pin = models.IntegerField(null=True, blank=True)
 
