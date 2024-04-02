0a55068af575db522541ef956c2066e1940d2a10
solo2112 <dawitabrham0021@gmail.com>
2024-04-03 01:14:12 +0300

    id field removed

--- core/models.py
+++ core/models.py
@@ -4,10 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     middle_name = models.CharField(max_length=50, blank=True, null=True)
-    username = models.EmailField(unique=True, primary_key=True)
+    username = models.EmailField(unique=True)
     verification_pin = models.IntegerField(null=True, blank=True)
     password_reset_pin = models.IntegerField(null=True, blank=True)
-    id = models.BigAutoField(unique=True)
 
     def __str__(self) -> str:
         return self.email
