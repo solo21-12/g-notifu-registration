9a94874a4ec6a915763f45235f15a909f255a575
solo2112 <dawitabrham0021@gmail.com>
2024-04-24 00:09:58 +0300

    notification admin update

--- notification/admin.py
+++ notification/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Notification
+
 # Register your models here.
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['message_content', 'notification_type',
+                    'priority_level', 'seen', 'created_at']
+    list_filter = ['created_at']
+    list_per_page = 10
+    ordering = ['created_at']
--- users/admin.py
+++ users/admin.py
@@ -1,25 +1,30 @@
 from django.contrib import admin
-from .models import Address,CompanyOwner, IndividualOwner,Owner
+from .models import Address, CompanyOwner, IndividualOwner, Owner
 from django.contrib.admin import ModelAdmin
 
 # Register your models here.
+
+
 @admin.register(Address)
 class Address(ModelAdmin):
     pass
+
 
 @admin.register(CompanyOwner)
 class CompanyOwners(ModelAdmin):
     list_per_page = 10
     list_select_related = ['user']
     ordering = ['company_name']
-    list_display = ['company_name', 'owner_type','contact']
-    search_fields =['company_name','contact']
+    list_display = ['company_name', 'owner_type',
+                    'contact']
+    search_fields = ['company_name', 'contact']
+
 
 @admin.register(IndividualOwner)
 class IndividualOwners(ModelAdmin):
     list_per_page = 10
     list_select_related = ['user']
-    list_display =['first_name', 'middle_name','last_name','owner_type','contact']
-    search_fields = ['user__first_name__istartswith', 'user__last_name__istartswith']
-    
-
+    list_display = ['first_name', 'middle_name', 'last_name',
+                    'owner_type', 'contact']
+    search_fields = ['user__first_name__istartswith',
+                     'user__last_name__istartswith']
