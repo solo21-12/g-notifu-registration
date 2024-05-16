f56a4af1fdfeef85ee446daec5d8fc4fb4b0e64f
solo2112 <dawitabrham0021@gmail.com>
2024-05-16 14:18:12 +0300

    url update 2

--- vehicle/apps.py
+++ vehicle/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class VehicleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'vehicle'
+
+# I want this thing to work serioulsy
