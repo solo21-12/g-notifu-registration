6b47f479996849db3b7f7c53aebee26a7e5fb871
solo2112 <dawitabrham0021@gmail.com>
2024-05-27 23:08:40 +0300

    insurance renewal update

--- documents/urls.py
+++ documents/urls.py
@@ -1,6 +1,6 @@
 from os import path
 from rest_framework.routers import DefaultRouter
-from .views import DocuemntViewSet, VehicleManagementViewUser, VehicleWithUser, RoadFundDocumentRenew
+from .views import DocuemntViewSet, VehicleManagementViewUser, VehicleWithUser, RoadFundDocumentRenew, InsuranceDocumentRenew
 
 router = DefaultRouter()
 router.register(r'', DocuemntViewSet, basename='document')
@@ -10,5 +10,7 @@ router.register(r'vehicle', VehicleWithUser,
                 basename='get_user_vehicle_documents')
 router.register(r'renew_road_fund', RoadFundDocumentRenew,
                 basename="renew_road_fund")
+router.register(r'renew_insurance', InsuranceDocumentRenew,
+                basename="renew_insurance")
 
 urlpatterns = router.urls
--- documents/views.py
+++ documents/views.py
@@ -46,7 +46,6 @@ class VehicleWithUser(viewsets.ViewSet):
         serializer = DocumentSerializer(user_documents, many=True)
         return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
 
-
 class RoadFundDocumentRenew(viewsets.ViewSet):
 
     '''This is the route handelr for renewing road fund document'''
@@ -123,5 +122,75 @@ class RoadFundDocumentRenew(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+class InsuranceDocumentRenew(viewsets.ViewSet):
+    '''This is a route to handle the renewal of third party insurance document'''
 
+    def retrieve(self, request, pk=None):
+        user_id = request.user.id
+        chassis_number = pk
 
+        cur_user = User.objects.get(id=user_id)
+        cur_vehicle = Vehicel.objects.get(chassis_number=chassis_number)
+
+        if cur_vehicle.owner != cur_user:
+            return Response({"Message": "The current users doesn't own the vehicle"}, status=status.HTTP_400_BAD_REQUEST)
+
+        url = 'http://localhost:8001'
+
+        result = helper.make_api_call(
+            f'{url}/insurance/get_payment_info/{chassis_number}/')
+
+        if result.status_code == 200:
+            return Response(result.json())
+
+        return Response({"Message": "Something went wrong"}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        """
+        Update the document for a vehicle owned by the current user.
+
+        Args:
+            request (Request): The HTTP request object containing data and user information.
+            pk (str): The primary key (chassis number) of the vehicle.
+
+        Returns:
+            Response: A Response object with the result of the update operation.
+        """
+        chassis_number = pk
+        transaction_code = request.data.get("transaction_code")
+        user_id = request.user.id
+
+        try:
+            cur_user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response({"Message": "User does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            cur_vehicle = Vehicel.objects.get(chassis_number=chassis_number)
+        except Vehicel.DoesNotExist:
+            return Response({"Message": "Vehicle does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if cur_vehicle.owner != cur_user:
+            return Response({"Message": "The current user doesn't own the vehicle"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            cur_doc = Document.objects.get(
+                vehicle=cur_vehicle, renewal_status=True, document_type=THIRD_PARTY_INSURANCE)
+        except Document.DoesNotExist:
+            return Response({"Message": "Current vehicle doesn't have an active document"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Outdate the current active document
+        outdated_success = helper.outdate_document(cur_doc.id)
+
+        if not outdated_success:
+            return Response({"Message": "Error occured"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Update the vehicle road fund document and create a new document
+        updated_doc = helper.update_document(
+            cur_vehicle, THIRD_PARTY_INSURANCE, transaction_code, insurance_company_name=cur_doc.insurance_company_name)
+
+        if not updated_doc:
+            return Response({"Message": "Error occured"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = DocumentSerializer(updated_doc)
+        return Response(serializer.data, status=status.HTTP_200_OK)
