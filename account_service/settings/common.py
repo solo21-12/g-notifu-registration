d802063f9f307b8a9fad9641d7902587182b5a37
solo2112 <dawitabrham0021@gmail.com>
2024-06-16 22:59:16 +0300

    guest mode init

--- account_service/settings/common.py
+++ account_service/settings/common.py
@@ -40,9 +40,8 @@ INSTALLED_APPS = [
     'notification.apps.NotificationConfig',
     'documents.apps.DocumentsConfig',
     'vehicle.apps.VehicleConfig',
-    'files.apps.FilesConfig'
-
-
+    'files.apps.FilesConfig',
+    'guest.apps.GuestConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +122,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-APPEND_SLASH=False
+APPEND_SLASH = False
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 REST_FRAMEWORK = {
