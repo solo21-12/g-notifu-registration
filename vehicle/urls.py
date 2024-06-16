2f7e4d58fc41123c51eec496377bee7c5d2bc7af
solo2112 <dawitabrham0021@gmail.com>
2024-06-16 10:08:12 +0300

    file generate document types

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import AddVehicleViewSet, ManageVehicleViewSet, VehicleListView
 router = DefaultRouter()
 # router.register(r'', VehicleViewSet, basename='vehicle')
-router.register(r'add_vehicle', AddVehicleViewSet, basename='add_vehicle')
 router.register(r'', ManageVehicleViewSet, basename='vehicel')
+router.register(r'add_vehicle', AddVehicleViewSet, basename='add_vehicle')
 router.register(r'owner_vehicle', VehicleListView, basename='owner_vehicle')
 urlpatterns = router.urls
